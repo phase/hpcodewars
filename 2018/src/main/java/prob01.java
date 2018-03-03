5383d68aa91cbe0b9837decbed3434b6933add1c
Jadon Fowler <jadonflower@gmail.com>
2018-03-03 00:20:24 -0800

    Getting ready for 2018
    woot woot
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

---
+++ 2018/src/main/java/prob01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class prob01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("Salutations, " + name + "! We are the Vista Eagles!");
+        in.close();
+    }
+}
---
+++ 2018/src/main/java/prob03.java
@@ -0,0 +1,5 @@
+public class prob03 {
+    public static void main(String[] args) {
+
+    }
+}
